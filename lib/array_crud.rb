bc95cd7df4ae00c3e8601c3d1657ca40b1e139ab
Valerie Manuel <valerie.manuel11@gmail.com>
2019-06-22 13:10:54 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  
+  []
 end
 
-def create_an_array
-  
+def create_an_array 
+  Cartoon = ["Sleepy","Happy","Dopey","Grumpy"]
 end
 
 def add_element_to_end_of_array(array, element)
